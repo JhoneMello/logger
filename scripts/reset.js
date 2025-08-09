c90da75bcc0f5d86dd609e5edaa59fd6939f5641
JHONE CORREA DE MELLO <jhonemello7559@digitalcollege.internal>
2025-08-09 09:04:48 -0300

    finalizando logica de recuperação de senha com js

--- scripts/reset.js
+++ scripts/reset.js
@@ -7,18 +7,20 @@
      var confirm = document.getElementById("confirm").value;
      
 
-     if(senha === confirm){
-         sessionStorage.setItem("password", senha)
-         alert("Sua senha foi redefinida com sucesso.");
-         window.location.href = "../pages/login.html";
+     if(senha != confirm){
+         alert("Certifique-se de que ambas as senhas digitadas são iguais.");
          return;
          
     }
 
+    sessionStorage.setItem("password", senha)
+         alert("Sua senha foi redefinida com sucesso.");
+         window.location.href = "../pages/login.html";
 
-    
-     alert("Certifique-se de que ambas as senhas digitadas são iguais.")
 
 });
+
+    
+     
 
 // sessionStorage.setItem("confirm", password);
